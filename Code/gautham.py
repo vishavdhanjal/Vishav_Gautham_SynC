90989f2c793e61a3f72b33694ea37e519b2736f4
vishavdhanjal <31722953+vishavdhanjal@users.noreply.github.com>
2020-03-06 12:20:57 +0530

    updater

---
+++ Code/gautham.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 12:19:22 2020
+
+@author: Vishavpreet
+"""
+
+#Load ECG
+
+#Preprocessing: NORMALIZATION, SMOOTHENING, AND BASELINE CORRECTION, PEAK Detection
